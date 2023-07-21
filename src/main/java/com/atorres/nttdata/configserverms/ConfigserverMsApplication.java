6fe5846b7b77db790a84a5e8e6c339aa04cf8f5e
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-20 19:56:00 -0500

    dev: commit init

---
+++ src/main/java/com/atorres/nttdata/configserverms/ConfigserverMsApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.configserverms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverMsApplication.class, args);
+	}
+
+}
